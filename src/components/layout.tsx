d035d435d6364d85aa6512b2531917b62ad8222d
caseyharvey <caseyharvey1@gmail.com>
2020-04-06 15:43:13 +1000

    add nav inside main

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Nav from '../components/Nav/Nav'
 import './layout.sass'
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
-      <main>{children}</main>
+      <main>
+        <Nav />
+        {children}
+      </main>
     </>
   )
 }
