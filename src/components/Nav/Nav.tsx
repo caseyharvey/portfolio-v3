cf95d0f81776a49354579adf36ca04b7338acbdf
caseyharvey <caseyharvey1@gmail.com>
2020-04-06 15:44:46 +1000

    changed style of nav and fixed css bugs

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -10,30 +10,28 @@ function Nav() {
   return (
     <nav>
       <ThemeToggle />
-      <Scrollspy
-        items={['home', 'about', 'projects', 'contact', 'footer']}
-        currentClassName="is-current"
-      >
-        <li className="arrows">
-          <a href="#home">
-            <UpArrow />
-          </a>
-        </li>
-        <li>
-          <a href="#about">about</a>
-        </li>
-        <li>
-          <a href="#projects">projects</a>
-        </li>
-        <li>
-          <a href="#contact">contact</a>
-        </li>
-        <li className="arrows">
-          <a href="#footer">
-            <DownArrow />
-          </a>
-        </li>
-      </Scrollspy>
+      <div className="links-container">
+        <Scrollspy
+          items={['home', 'about', 'projects', 'contact', 'footer']}
+          currentClassName="is-current"
+        >
+          <li>
+            <a href="#home">{'<'}</a>
+          </li>
+          <li>
+            <a href="#about">about</a>
+          </li>
+          <li>
+            <a href="#projects">projects</a>
+          </li>
+          <li>
+            <a href="#contact">contact</a>
+          </li>
+          <li>
+            <a href="#footer">{'>'}</a>
+          </li>
+        </Scrollspy>
+      </div>
     </nav>
   )
 }
