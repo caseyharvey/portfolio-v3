7fa38097996a3f942c920b48a89ae92db26ff18c
caseyharvey <caseyharvey1@gmail.com>
2020-04-07 15:43:25 +1000

    add optional 3rd line and push background to left

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -4,13 +4,15 @@ import './heading.sass'
 interface Props {
   heading: string
   tagLine: string
+  info?: string
 }
 
-function Heading({ heading, tagLine }: Props) {
+function Heading({ heading, tagLine, info }: Props) {
   return (
     <div className="heading-container">
       <h1>{heading}</h1>
       <span>{tagLine}</span>
+      <p>{info}</p>
     </div>
   )
 }
