c536b01d1cf863aa192631a598d821155e7da88a
caseyharvey <caseyharvey1@gmail.com>
2020-04-07 21:15:04 +1000

    add blog link to footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Footer() {
   return (
     <section id="footer">
-      <div className="section-container">FOOTER SECTION</div>
+      <div className="footer-container">
+        <a href="https://blog.caseys.tech">Check out my blog</a>
+      </div>
     </section>
   )
 }
