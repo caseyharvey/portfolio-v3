3e78bb963f50c2bff9b546469688396de3f62c1a
caseyharvey <caseyharvey1@gmail.com>
2020-04-08 12:52:03 +1000

    refactor nav and remove scroll-spy

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,33 +1,29 @@
 import React from 'react'
-import Scrollspy from 'react-scrollspy'
 import ThemeToggle from '../ThemeToggle/ThemeToggle'
 
 function Nav() {
   return (
     <nav>
-      <ThemeToggle />
-      <div className="nav-container">
-        <Scrollspy
-          items={['home', 'about', 'projects', 'contact', 'footer']}
-          currentClassName="is-current"
-        >
-          <li>
-            <a href="#home">{'>>>'}</a>
-          </li>
-          <li>
-            <a href="#about">about</a>
-          </li>
-          <li>
-            <a href="#projects">projects</a>
-          </li>
-          <li>
-            <a href="#contact">contact</a>
-          </li>
-          <li>
-            <a href="#footer">{'<<<'}</a>
-          </li>
-        </Scrollspy>
-      </div>
+      <ul>
+        <li>
+          <ThemeToggle />
+        </li>
+        <li>
+          <a href="#home">{'>'}</a>
+        </li>
+        <li>
+          <a href="#about">about</a>
+        </li>
+        <li>
+          <a href="#projects">projects</a>
+        </li>
+        <li>
+          <a href="#contact">contact</a>
+        </li>
+        <li>
+          <a href="https://blog.caseys.tech">blog</a>
+        </li>
+      </ul>
     </nav>
   )
 }
