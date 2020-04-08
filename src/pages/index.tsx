e720c081982779dcc8fc6a936d5a23a7e883e45c
caseyharvey <caseyharvey1@gmail.com>
2020-04-08 12:52:46 +1000

    remove footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
-
 import About from '../components/Sections/About'
 import Contact from '../components/Sections/Contact'
-import Footer from '../components/Sections/Footer'
 import Home from '../components/Sections/Home'
 import Projects from '../components/Sections/Projects'
 import SEO from '../components/seo'
@@ -11,12 +9,10 @@ import SEO from '../components/seo'
 const IndexPage = () => (
   <Layout>
     <SEO title="caseys.tech | The portfolio of Casey Harvey" />
-
     <Home />
     <About />
     <Projects />
     <Contact />
-    <Footer />
   </Layout>
 )
 
