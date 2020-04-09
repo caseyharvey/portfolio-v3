374ca78c7771a9fa93b73aed2c984a14a5707ee4
caseyharvey <caseyharvey1@gmail.com>
2020-04-09 20:42:16 +1000

    add two more porjects

--- src/components/Project/projectsArray.tsx
+++ src/components/Project/projectsArray.tsx
@@ -19,5 +19,25 @@ const projectArray = [
     info:
       'This is pig latin converter. After seeing many people attempt to program this idea without truly succeeding, I wrote an algorithm to handle the conversion properly. The first attempt was done a while ago and I have since refactored it and turned in into a react app.',
   },
+  {
+    id: 3,
+    name: 'caseys.tech 2.0',
+    tagline: 'version 2 of my portfolio',
+    url: 'https://v2.caseys.tech/',
+    git: 'https://github.com/caseyharvey/v2-caseys.tech/tree/master/src',
+    extra: 'react react-hook-form gatsby',
+    info:
+      'I started Learning gatsby and knew it would make a huge difference to my page so I set out to rebuild it. This approach gave me chance to work on SEO and I added an option for light and dark mode as well. ',
+  },
+  {
+    id: 4,
+    name: 'caseys.tech 1.0',
+    tagline: 'version 1 of my portfolio',
+    url: 'https://v1.caseys.tech/',
+    git: 'https://github.com/caseyharvey/v1-caseys.tech/tree/master/src',
+    extra: 'react react-hook-form react-router',
+    info:
+      'This is my first attempt at at a portfolio. Through the process i learnt so much, by the end I knew I could do a much better job. So I did',
+  },
 ]
 export default projectArray
